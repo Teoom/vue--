e58f18d57e6cf120e66202241111b7f36268b015
Teoom <ararama199915@gmail.com>
2025-01-15 14:51:18 +0700

    test

---
+++ index.js
@@ -0,0 +1,41 @@
+// const options = {
+//   data() {
+//     return {
+//       width: 100,
+//       height: 1
+
+//     }
+//   },
+//   methods: {
+//     setMessage(event) {
+//       this.message = event.target.value;
+//     },
+//     increment() {
+//       console.log(this.aaa === aaa)
+//       console.log(this.aaa)
+//       this.count += 1;
+//     },
+//     decrement() {
+//       if(this.count !== 0) {
+//         this.count -= 1;
+//       }
+//     },
+//     hi() {
+//       return "Hi"
+//     },
+//     test(a) {
+//       console.log(a)
+//     }
+//   },
+//   mounted() {
+//     console.log(this.$root)
+//   },
+
+// }
+
+// const App = Vue.createApp(options)
+// console.log(App)
+
+// App.mount('#app');
+
+console.log("test", import.meta.url)
